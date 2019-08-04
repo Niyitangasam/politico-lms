f44a15c9d269aface57d5a949da2ea0c21f55651
Niyitangasam <niyitangasam@gmail.com>
2019-08-04 21:52:46 +0200

    chore(setup): Setup redux
    - Setup redux
    - Setup react router
    - Configure folder structure
    - Configure redux
    
    [Finishes]

---
+++ src/components/login.jsx
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+
+class Login extends Component {
+  render() {
+    return (
+      <div id="content">
+        <div id="header">
+          <p id="top" />
+          <h1 id="logo">Politico</h1>
+          <ul id="menu">
+            <li>
+              <a href="index.html">Log in</a>
+            </li>
+            <li>
+              <a href="signup.html">Sign Up</a>
+            </li>
+          </ul>
+        </div>
+        <h1 id="top_form">Welcome To Politico</h1>
+
+        <div className="form">
+          <div className="field-wrap">
+            <h1>Please login!</h1>
+          </div>
+          <div className="field-wrap">
+            <input
+              placeholder="Email Address *"
+              type="email"
+              required
+              autocomplete="off"
+            />
+          </div>
+
+          <div className="field-wrap">
+            <input
+              placeholder="password *"
+              type="password"
+              required
+              autocomplete="off"
+            />
+          </div>
+
+          <p className="forgot">
+            <a href="reset.html">Forgot Password?</a>
+          </p>
+          <button className="button button-block">
+            <a href="user/user.html">Log In</a>
+          </button>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Login;
---
+++ src/store.js
@@ -0,0 +1,17 @@
+import { createStore, applyMiddleware } from 'redux';
+import promise from 'redux-promise-middleware';
+import thunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import rootReducer from './reducers';
+
+let configureStore;
+
+configureStore = () => {
+  return createStore(
+    rootReducer,
+    {},
+    composeWithDevTools(applyMiddleware(thunk, promise))
+  );
+};
+
+export default configureStore;
