6ebf11268d7c8c8453b5e7d7824a4d04c27e8bfa
Sam <niyitangasam@gmail.com>
2019-08-05 09:07:21 +0200

    ft(reset): Add reset password functionality (#6)
    - Add reset functionality
    - Add reset password component and fix url

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './components/login';
 import SignUp from './components/signUp';
+import Reset from './components/reset';
 import './assets/css/style';
 
 const store = configureStore();
@@ -16,6 +17,7 @@ class App extends Component {
           <Switch>
             <Route exact path="/" render={props => <Login {...props} />} />
             <Route exact path="/signup" render={props => <SignUp {...props} />} />
+            <Route exact path="/reset" render={props => <Reset {...props} />} />
           </Switch>
         </BrowserRouter>
       </Provider>
